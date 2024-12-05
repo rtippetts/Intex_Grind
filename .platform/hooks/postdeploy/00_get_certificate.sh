9dce8e86350ca53ed583993cd4c71cf288221bb4
bcarter54 <carterbryce54@gmail.com>
2024-12-05 09:31:52 -0700

    back to is404

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d turtleshelter.net --nginx --agree-tos --email thecoolbears1.8@gmail.com
+sudo certbot -n -d turtleshelter.is404.net --nginx --agree-tos --email thecoolbears1.8@gmail.com
