620d845bbc032ca0a63d329da1540c9b2729c03f
bcarter54 <carterbryce54@gmail.com>
2024-12-04 17:12:21 -0700

    got rid of extra stuff

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,4 +1,2 @@
-
 #!/usr/bin/env bash
-# Place in .platform/hooks/postdeploy directory
 sudo certbot -n -d turtleshelterproject.us-east-2.elasticbeanstalk.com --nginx --agree-tos --email thecoolbears1.8@gmail.com
